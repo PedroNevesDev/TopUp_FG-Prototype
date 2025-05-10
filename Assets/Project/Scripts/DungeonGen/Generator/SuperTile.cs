02e460e2ebdc5d05caa042c10a72804be7f3dd80
PedroNeves <133124266+PedroNevesDev@users.noreply.github.com>
2025-05-10 07:40:15 +0100

    Revert

--- Assets/Project/Scripts/DungeonGen/Generator/SuperTile.cs
+++ Assets/Project/Scripts/DungeonGen/Generator/SuperTile.cs
@@ -21,7 +21,6 @@ public class SuperTile : MonoBehaviour
     public GameObject forwardWall;
     public GameObject backWall;
     public GameObject floor;
-    public GameObject ceiling;
 
     // Cache if the walls have been disabled to avoid redundant SetActive calls
     private bool rightWallDisabled = false;
@@ -78,8 +77,6 @@ public void PlaceDecor(GameObject prefab, DecorType decorType)
 
     obj.transform.position = randomObj.transform.position;
     occupied.Add(randomObj,obj);
-    if(!obj.TryGetComponent(out Enemy enemy))
-        obj.isStatic = true;
 }
 
     public void Connect(SuperTile other, Vector3Int direction)
